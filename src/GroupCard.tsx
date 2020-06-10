aea4f41c10cd879819590dc91dd35d5d1c3fe9a9
GerA <lpgera@gmail.com>
2020-06-10 22:33:00 +0200

    add more mock UI

--- src/GroupCard.tsx
+++ src/GroupCard.tsx
@@ -1,17 +1,29 @@
 import React, { useState } from 'react'
-import { Card, Collapse, Divider, Slider } from 'antd'
+import { Card, Col, Collapse, Divider, Row, Slider, Switch } from 'antd'
 
 const GroupCard = () => {
   const [value, setValue] = useState(0)
 
   return (
     <Card title={'Bedroom'}>
-      <Slider
-        min={0}
-        max={100}
-        value={value}
-        onChange={(newValue) => setValue(newValue as number)}
-      />
+      <Row align="middle">
+        <Col flex="50px">
+          <Switch
+            size="small"
+            checked={value > 0}
+            onChange={(newValue) => setValue(newValue ? 100 : 0)}
+          />
+        </Col>
+        <Col flex="auto">
+          <Slider
+            style={{ marginTop: '12px' }}
+            min={0}
+            max={100}
+            value={value}
+            onChange={(newValue) => setValue(newValue as number)}
+          />
+        </Col>
+      </Row>
       <Collapse style={{ marginTop: '32px' }}>
         <Collapse.Panel header="Devices" key="1">
           {[...Array(3)].map((_, index, array) => (
