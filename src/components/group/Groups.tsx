382d0da6a3deac0f32c7987be52a80b72e5f2e3c
GerA <lpgera@gmail.com>
2020-06-20 22:23:27 +0200

    add initial loading and error states

--- src/components/group/Groups.tsx
+++ src/components/group/Groups.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import { Col, Row } from 'antd'
-import GroupCard from './GroupCard'
+import { Card, Col, Result, Row, Skeleton } from 'antd'
 import { useQuery } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
+import GroupCard from './GroupCard'
 import { GroupsQuery } from './Groups.types.gen'
 
+const columnSizes = {
+  xs: 24,
+  sm: 12,
+  md: 8,
+  lg: 6,
+}
+
 const Groups = () => {
-  const { data, refetch } = useQuery<GroupsQuery>(gql`
+  const { data, refetch, loading, error } = useQuery<GroupsQuery>(gql`
     query Groups {
       groups {
         id
@@ -27,11 +34,23 @@ const Groups = () => {
 
   return (
     <Row gutter={[16, 16]}>
-      {groups.map((group) => (
-        <Col key={group.id} xs={24} sm={12} md={8} lg={6}>
-          <GroupCard {...group} refetch={refetch} />
+      {loading ? (
+        <Col key={'loading'} {...columnSizes}>
+          <Card>
+            <Skeleton active={true} />
+          </Card>
         </Col>
-      ))}
+      ) : error ? (
+        <Col xs={24}>
+          <Result status="error" title="Error" subTitle={error.message} />
+        </Col>
+      ) : (
+        groups.map((group) => (
+          <Col key={group.id} {...columnSizes}>
+            <GroupCard {...group} refetch={refetch} />
+          </Col>
+        ))
+      )}
     </Row>
   )
 }
