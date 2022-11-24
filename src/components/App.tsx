d0f8b2cea2648abd15f4869b264043d99f4920ba
GerA <lpgera@gmail.com>
2022-11-24 18:11:52 +0100

    migrate to ant design v5

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import 'antd/dist/antd.dark.min.css'
 import ApolloProvider from './ApolloProvider'
 import { AuthProvider } from './AuthContext'
 import Frame from './Frame'
+import { ConfigProvider, theme } from 'antd'
+import 'antd/dist/reset.css'
 
 function App() {
   return (
     <AuthProvider>
       <ApolloProvider>
-        <Frame />
+        <ConfigProvider
+          theme={{
+            algorithm: theme.darkAlgorithm,
+          }}
+        >
+          <Frame />
+        </ConfigProvider>
       </ApolloProvider>
     </AuthProvider>
   )
