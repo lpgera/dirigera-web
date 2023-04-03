5425adab0616daaafd4f0827b084984867176128
GerA <lpgera@gmail.com>
2023-04-03 23:12:49 +0200

    update stories to csf 3

--- src/components/Group.stories.tsx
+++ src/components/Group.stories.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { Meta, StoryObj } from '@storybook/react'
 import 'antd/dist/reset.css'
 import Group from './Group'
 import { AccessoryType } from '../graphql.types'
+import Frame from './Frame'
 
 export default {
   title: 'Group states',
@@ -13,137 +14,143 @@ export default {
     id: 1,
     isDeviceListDefaultOpen: true,
   },
-} as ComponentMeta<typeof Group>
+} as Meta<typeof Group>
 
-const Template: ComponentStory<typeof Group> = (args) => <Group {...args} />
+type Story = StoryObj<typeof Frame>
 
-export const Empty = Template.bind({})
-Empty.args = {
-  name: 'Empty group',
-  accessories: [],
+export const Empty: Story = {
+  args: {
+    name: 'Empty group',
+    accessories: [],
+  },
 }
 
-export const Bulb = Template.bind({})
-Bulb.args = {
-  name: 'Group with bulb',
-  accessories: [
-    {
-      id: 1,
-      name: 'Bulb',
-      type: AccessoryType.Lightbulb,
-      alive: true,
-      onOff: true,
-      dimmer: 100,
-      battery: null,
-      colorTemperature: 60,
-    },
-    {
-      id: 2,
-      name: 'Remote',
-      type: AccessoryType.Remote,
-      alive: true,
-      onOff: null,
-      dimmer: null,
-      battery: 87,
-      colorTemperature: null,
-    },
-    {
-      id: 3,
-      name: 'Motion sensor',
-      type: AccessoryType.MotionSensor,
-      alive: true,
-      onOff: null,
-      dimmer: null,
-      battery: 74,
-      colorTemperature: null,
-    },
-  ],
+export const Bulb: Story = {
+  args: {
+    name: 'Group with bulb',
+    accessories: [
+      {
+        id: 1,
+        name: 'Bulb',
+        type: AccessoryType.Lightbulb,
+        alive: true,
+        onOff: true,
+        dimmer: 100,
+        battery: null,
+        colorTemperature: 60,
+      },
+      {
+        id: 2,
+        name: 'Remote',
+        type: AccessoryType.Remote,
+        alive: true,
+        onOff: null,
+        dimmer: null,
+        battery: 87,
+        colorTemperature: null,
+      },
+      {
+        id: 3,
+        name: 'Motion sensor',
+        type: AccessoryType.MotionSensor,
+        alive: true,
+        onOff: null,
+        dimmer: null,
+        battery: 74,
+        colorTemperature: null,
+      },
+    ],
+  },
 }
 
-export const Driver = Template.bind({})
-Driver.args = {
-  name: 'Group with LED driver',
-  accessories: [
-    {
-      id: 1,
-      name: 'Driver',
-      type: AccessoryType.Lightbulb,
-      alive: true,
-      onOff: true,
-      dimmer: 50,
-      battery: null,
-      colorTemperature: null,
-    },
-  ],
+export const Driver: Story = {
+  args: {
+    name: 'Group with LED driver',
+    accessories: [
+      {
+        id: 1,
+        name: 'Driver',
+        type: AccessoryType.Lightbulb,
+        alive: true,
+        onOff: true,
+        dimmer: 50,
+        battery: null,
+        colorTemperature: null,
+      },
+    ],
+  },
 }
 
-export const Plug = Template.bind({})
-Plug.args = {
-  name: 'Group with plug',
-  accessories: [
-    {
-      id: 1,
-      name: 'Plug',
-      type: AccessoryType.Plug,
-      alive: true,
-      onOff: true,
-      dimmer: null,
-      battery: null,
-      colorTemperature: null,
-    },
-  ],
+export const Plug: Story = {
+  args: {
+    name: 'Group with plug',
+    accessories: [
+      {
+        id: 1,
+        name: 'Plug',
+        type: AccessoryType.Plug,
+        alive: true,
+        onOff: true,
+        dimmer: null,
+        battery: null,
+        colorTemperature: null,
+      },
+    ],
+  },
 }
 
-export const Inaccessible = Template.bind({})
-Inaccessible.args = {
-  name: 'Group with inaccessible devices',
-  accessories: [
-    {
-      id: 1,
-      name: 'Bulb',
-      type: AccessoryType.Lightbulb,
-      alive: false,
-      onOff: true,
-      dimmer: 100,
-      battery: null,
-      colorTemperature: 60,
-    },
-    {
-      id: 2,
-      name: 'Driver',
-      type: AccessoryType.Lightbulb,
-      alive: false,
-      onOff: true,
-      dimmer: 50,
-      battery: null,
-      colorTemperature: null,
-    },
-    {
-      id: 3,
-      name: 'Plug',
-      type: AccessoryType.Plug,
-      alive: false,
-      onOff: true,
-      dimmer: null,
-      battery: null,
-      colorTemperature: null,
-    },
-  ],
+export const Inaccessible: Story = {
+  args: {
+    name: 'Group with inaccessible devices',
+    accessories: [
+      {
+        id: 1,
+        name: 'Bulb',
+        type: AccessoryType.Lightbulb,
+        alive: false,
+        onOff: true,
+        dimmer: 100,
+        battery: null,
+        colorTemperature: 60,
+      },
+      {
+        id: 2,
+        name: 'Driver',
+        type: AccessoryType.Lightbulb,
+        alive: false,
+        onOff: true,
+        dimmer: 50,
+        battery: null,
+        colorTemperature: null,
+      },
+      {
+        id: 3,
+        name: 'Plug',
+        type: AccessoryType.Plug,
+        alive: false,
+        onOff: true,
+        dimmer: null,
+        battery: null,
+        colorTemperature: null,
+      },
+    ],
+  },
 }
 
-export const ShortcutButtonGroup = Template.bind({})
-ShortcutButtonGroup.args = {
-  name: 'Shortcut button group',
-  accessories: [
-    {
-      id: 1,
-      name: 'TRADFRI SHORTCUT Button',
-      type: AccessoryType.Remote,
-      alive: true,
-      onOff: null,
-      dimmer: null,
-      battery: 87,
-      colorTemperature: null,
-    },
-  ],
+export const ShortcutButtonGroup: Story = {
+  args: {
+    name: 'Shortcut button group',
+    accessories: [
+      {
+        id: 1,
+        name: 'TRADFRI SHORTCUT Button',
+        type: AccessoryType.Remote,
+        alive: true,
+        onOff: null,
+        dimmer: null,
+        battery: 87,
+        colorTemperature: null,
+      },
+    ],
+  },
 }
