6e4f6a4dc1ebab2d3b8c1350bbe2634d58f8d387
GerA <lpgera@gmail.com>
2023-04-16 13:24:26 +0200

    add ws authorization logic to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,10 +4,12 @@ import http from 'http'
 import tsEnv from '@lpgera/ts-env'
 import { Server } from 'ws'
 import { json } from 'body-parser'
+import type { Socket } from 'net'
 import { expressMiddleware } from '@apollo/server/express4'
 import { getClient } from './dirigera'
 import { apolloServer } from './graphql/server'
 import { getContextFunction } from './graphql/context'
+import { verify } from './jwt'
 
 const app = express()
 app.use(
@@ -20,7 +22,25 @@ app.get('/', function (_, res) {
 })
 
 const httpServer = http.createServer(app)
-const wss = new Server({ server: httpServer })
+const wss = new Server({ noServer: true })
+
+httpServer.on(
+  'upgrade',
+  (request: http.IncomingMessage, socket: Socket, head: Buffer) => {
+    const url = new URL(request.url ?? '', `http://${request.headers.host}`)
+
+    if (!verify(url.searchParams.get('token') ?? '')) {
+      socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n')
+      socket.destroy()
+      return
+    }
+
+    wss.handleUpgrade(request, socket, head, function done(ws) {
+      wss.emit('connection', ws, request)
+    })
+  }
+)
+
 const port = tsEnv.number('PORT') ?? 4000
 
 async function start() {
