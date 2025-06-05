1687bfe4fd6f0ef26afb2999a3da5c22673221ae
GerA <lpgera@gmail.com>
2025-06-05 14:18:39 +0200

    remove unnecessary addon

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,6 +1,6 @@
 export default {
   stories: ['../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-links', 'storybook-addon-remix-react-router'],
+  addons: ['storybook-addon-remix-react-router'],
   docs: {
     autodocs: 'tag',
   },
